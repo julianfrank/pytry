37e1df79f91e009c7dbbd94c9a24e8616d8e2dac
jf laptop ubuntu <jfempire@yahoo.co.uk>
2019-06-01 15:59:40 +0530

    setup for linux as well!

--- chap2.py
+++ chap2.py
@@ -1,8 +1,8 @@
+from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
+
 (train_images,train_labels),(test_images,test_labels)=mnist.load_data()
 
-from tensorflow.keras import models
-from tensorflow.keras import layers
 
 network = models.Sequential()
 network.add(layers.Dense(512,activation='relu',input_shape=(28*28,)))
